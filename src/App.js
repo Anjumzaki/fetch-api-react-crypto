a9c7889682bf377b9c885c6133252b52c741f36e
anjumzaki <anjumzaki8@gmail.com>
2018-11-28 23:56:13 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -3,25 +3,53 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+      isLoading: true,
+    }
+  }
+  fetchUsers() {
+    // Where we're fetching data from
+    fetch(`https://api.coinmarketcap.com/v1/ticker/?limit=2000`)
+      // We get the API response and receive data in JSON format...
+      .then(response => response.json())
+      // ...then we update the users state
+      .then(data =>
+        this.setState({
+          users: data,
+          isLoading: false,
+          error: null,
+        })
+      )
+      // Catch any errors we hit and update the app
+      .catch(error => this.setState({ error, isLoading: false }));
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    
+    
+      <h1>Random User</h1>
+      // Display a message if we encounter an error
+      {error ? <p>{error.message}</p> : null}
+      // Here's our data check
+      {!isLoading ? (
+        users.map(user => {
+          const { username, name, email } = user;
+          return (
+            <div key={username}>
+              <p>Name: {name}</p>
+              <p>Email Address: {email}</p>
+              <hr />
+            </div>
+          );
+        })
+      // If there is a delay in data, let's let the user know it's loading
+      ) : (
+        <h3>Loading...</h3>
+      )}
   }
 }
 
